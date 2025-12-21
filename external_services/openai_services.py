6cad096ba53628fe1555fcc40ebe6e468487733d
big <inetsmol@gmail.com>
2025-12-21 14:31:45 +0300

    refactor: update OpenAI API calls in openai_services.py for improved response handling

--- external_services/openai_services.py
+++ external_services/openai_services.py
@@ -34,48 +34,41 @@ async def openai_text_to_speech(text: str):
 def openai_gpt_add_space(text):
     if location == 'ja-JP':
         client = OpenAI() if proxy_url is None or proxy_url == "" else OpenAI(http_client=httpx.Client(proxy=proxy_url))
-        completion = client.chat.completions.create(
+
+        response = client.responses.create(
             model="gpt-5-nano",
-            # model="gpt-3.5-turbo",
-            # model="gpt-4o-mini",
-            messages=[{"role": "user",
-                       "content": f"add spaces between words in the following text {text} return only spaced text"}]
+            input=f"add spaces between words in the following text {text} return only spaced text"
         )
-        return completion.choices[0].message.content
+        return response.output_text
     else:
         return text
 
 
 def openai_gpt_translate(text):
     client = OpenAI() if proxy_url is None or proxy_url == "" else OpenAI(http_client=httpx.Client(proxy=proxy_url))
-    completion = client.chat.completions.create(
+    response = client.responses.create(
         model="gpt-5-nano",
-        # model="gpt-4o-mini",
-        # model="gpt-3.5-turbo",
-        messages=[{"role": "user",
-                   "content": f"translate this text into Russian: {text}. answer only with translation"}]
+        input=f"translate this text into Russian: {text}. answer only with translation"
     )
-    return completion.choices[0].message.content
+    return response.output_text
 
 
 def openai_gpt_get_phrase_from_text(text):
     client = OpenAI() if proxy_url is None or proxy_url == "" else OpenAI(http_client=httpx.Client(proxy=proxy_url))
-    completion = client.chat.completions.create(
+    response = client.responses.create(
         model="gpt-5-nano",
-        # model="gpt-3.5-turbo",
-        messages=[{"role": "user",
-                   "content": f"выбери из текста 5 фраз из 2-3 слов содержащих прилагательные и глаголы. фразы не "
-                              f"должны содержать имена собственные. Текст: {text}. составь пары фраза-перевод на "
-                              f"русский. в ответ пришли только полученные фразы. пример: たくさん降っています - много идет"}]
+        input=f"выбери из текста 5 фраз из 2-3 слов содержащих прилагательные и глаголы. фразы не "
+              f"должны содержать имена собственные. Текст: {text}. составь пары фраза-перевод на "
+              f"русский. в ответ пришли только полученные фразы. пример: たくさん降っています - много идет"
     )
-    return completion.choices[0].message.content
+    return response.output_text
 
 
 if __name__ == '__main__':
     # api_key = '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'
     location = 'ja-JP'
-    # print(openai_gpt_add_space('状況を話して苦情を言い最後にリクエストを言う'))
+    print(openai_gpt_add_space('状況を話して苦情を言い最後にリクエストを言う'))
 
-    print(openai_gpt_translate('状況を話して苦情を言い最後にリクエストを言う'))
+    # print(openai_gpt_translate('状況を話して苦情を言い最後にリクエストを言う'))
 
     # print(openai_gpt_translate('状況を話して苦情を言い最後にリクエストを言う'))
